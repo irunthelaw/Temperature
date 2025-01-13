9888310a8391e093c756dcc520a726510c47906e
irunthelaw <2026phillips.junior@fcboe.org>
2025-01-13 13:46:58 -0500

    Create progress.py

---
+++ progress.py
@@ -0,0 +1,6 @@
+while True:
+    print("Pressure: %.2f hPa" % sensor.pressure)
+    print("Temperature: %.2f C" % sensor.temperature)
+    print("Humidity: %.2f %% rH" % sensor.relative_humidity)
+    print("\n------------------------------------------------\n")
+    sleep(1)
